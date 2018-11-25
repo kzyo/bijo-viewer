13012011fc417f23312f256f1e6db5acece0b43f
kzyo <kzy.onzw@gmail.com>
2018-11-25 18:25:39 +0900

    [change migration file] config BijoImage

--- db/migrate/20181125091634_create_bijo_images.rb
+++ db/migrate/20181125091634_create_bijo_images.rb
@@ -1,10 +1,10 @@
 class CreateBijoImages < ActiveRecord::Migration[5.2]
   def change
     create_table :bijo_images do |t|
-      t.integer :tweet_id
-      t.integer :width
-      t.integer :height
-      t.string :url
+      t.integer :tweet_id, limit: 8, null: false
+      t.integer :width, null: false
+      t.integer :height, null: false
+      t.string :url, null: false
 
       t.timestamps
     end
