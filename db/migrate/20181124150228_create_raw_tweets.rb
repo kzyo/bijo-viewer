24f6d6fcb87c38587a962118b40fc5c9874ba307
kzyo <kzy.onzw@gmail.com>
2018-11-25 10:51:35 +0900

    [change migration file] set RawTweet.tweet_id bigint and not null

--- db/migrate/20181124150228_create_raw_tweets.rb
+++ db/migrate/20181124150228_create_raw_tweets.rb
@@ -1,7 +1,7 @@
 class CreateRawTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :raw_tweets do |t|
-      t.integer :tweet_id
+      t.integer :tweet_id, limit: 8, null: false
 
       t.timestamps
     end
