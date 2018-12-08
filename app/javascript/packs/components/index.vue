3d32cb18277769ff63bd776e29ca4ecb89f030fe
kzyo <kzy.onzw@gmail.com>
2018-12-08 18:45:40 +0900

    get first bijo_images from api

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -8,11 +8,18 @@
 
 
 <script>
+  import axios from 'axios';
   export default {
     data: function () {
      return {
-       tweet_images: [{"tweet_id":1066653158779355136,"width":499,"height":750,"url":"http://pbs.twimg.com/media/Ds17bETU4AABnR2.jpg"},{"tweet_id":1066653912193630208,"width":750,"height":936,"url":"http://pbs.twimg.com/media/Dszq7IUVsAA8mBy.jpg"},{"tweet_id":1066653912193630208,"width":682,"height":1024,"url":"http://pbs.twimg.com/media/Dszq7IVU0AE4KDI.jpg"},{"tweet_id":1066653912193630208,"width":682,"height":1024,"url":"http://pbs.twimg.com/media/Dszq7ITU8AAhMq6.jpg"},{"tweet_id":1066653912193630208,"width":557,"height":744,"url":"http://pbs.twimg.com/media/Dszq7ITVsAA4Y8e.jpg"},{"tweet_id":1066654409998839808,"width":750,"height":936,"url":"http://pbs.twimg.com/media/Dszq7IUVsAA8mBy.jpg"},{"tweet_id":1066654409998839808,"width":682,"height":1024,"url":"http://pbs.twimg.com/media/Dszq7IVU0AE4KDI.jpg"},{"tweet_id":1066654409998839808,"width":682,"height":1024,"url":"http://pbs.twimg.com/media/Dszq7ITU8AAhMq6.jpg"},{"tweet_id":1066654409998839808,"width":557,"height":744,"url":"http://pbs.twimg.com/media/Dszq7ITVsAA4Y8e.jpg"}]
+       tweet_images: []
      }
+    },
+    created: function () {
+      axios.get('/api/bijo_images')
+      .then((res) => {
+        this.tweet_images = res.data.bijo_images
+      })
     }
   }
 </script>
