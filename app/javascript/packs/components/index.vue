3da794dc66186053cb43bc59488db74c7662a2aa
kzyo <kzy.onzw@gmail.com>
2018-12-12 22:40:28 +0900

    add loading image

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -1,25 +1,36 @@
 <template>
+<div>
   <ul id="tiles">
     <li v-for="image in tweet_images">
       <img v-bind:src="image.url" v-bind:width="image.width + 'px'" v-bind:height="image.height + 'px'" />
     </li>
   </ul>
+  <div id="loading">
+    <img src="/loading.gif" alt="loading">
+  </div>
+</div>
 </template>
 
 
 <script>
   import axios from 'axios';
+
   export default {
     data: function () {
      return {
        tweet_images: []
      }
     },
-    created: function () {
-      axios.get('/api/bijo_images')
-      .then((res) => {
-        this.tweet_images = res.data.bijo_images
-      })
+    methods: {
+      get_images: function(){
+        axios.get('/api/bijo_images')
+        .then((res) => {
+          this.tweet_images = this.tweet_images.concat(res.data['bijo_images']);
+        });
+      },
+    },
+    created: function() {
+      this.get_images();
     }
   }
 </script>
